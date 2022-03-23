1e94dd5a836d7dc7a22d90315257781d9031acec
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2022-03-23 10:50:57 +0100

    Add files via upload

---
+++ AUKCE/JS/data.js
@@ -0,0 +1,26 @@
+const data = {
+  p001: {
+    name: "rychlost větru",
+    desc: "abstrakce větru v uzav. prostoru",
+    author: "Jakub Hanykyr",
+    images: ["art_001.jpg"],
+  },
+  p002: {
+    name: "Vesmír",
+    desc: "socha představující nekonečnost",
+    author: "Jiři Hanyk",
+    images: ["fanart.png"],
+  },
+  p003: {
+    name: "Slunce",
+    desc: "Alegorie na realitu",
+    author: "Jonas Safranek",
+    images: ["ART_002.jpg"],
+  },
+  p004: {
+    name: "Hněv",
+    desc: "Pocit",
+    author: "Jonas Safranek",
+    images: ["OIF.jpg"],
+  },
+};
